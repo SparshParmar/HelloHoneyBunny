1c62e46bf0beffa67f3db4f3af09fc95bae63e9f
sparsh_parmar <sparshparmar444@gmail.com>
2020-09-24 16:00:44 +0200

    feat: add Auth with REDUX

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import jwtDecode from 'jwt-decode';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
+import Axios from 'axios';
 const theme = createMuiTheme(themeFile);
 
 let authenticated;
@@ -22,12 +25,13 @@ const token = localStorage.FBIdToken;
 
 if (token) {
   const decodedToken = jwtDecode(token);
-  console.log(decodedToken.exp * 1000);
   if (decodedToken.exp * 1000 < Date.now()) {
     window.location.href = '/login';
-    authenticated = false;
+    store.dispatch(logoutUser());
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    Axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
 class App extends Component {
@@ -40,18 +44,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <AuthRoute
-                  exact
-                  path="/login"
-                  component={login}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/signUp"
-                  component={signUp}
-                  authenticated={authenticated}
-                />
+                <AuthRoute exact path="/login" component={login} />
+                <AuthRoute exact path="/signUp" component={signUp} />
               </Switch>
             </div>
           </Router>
