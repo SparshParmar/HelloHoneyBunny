6446e9109679d887dead28555710270ac5593b66
sparsh_parmar <sparshparmar444@gmail.com>
2020-09-24 18:35:11 +0200

    feat: add Profile section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import axios from 'axios';
 import Scream from '../components/Scream';
+import Profile from '../components/Profile';
 
 class home extends Component {
   state = {
@@ -28,9 +29,13 @@ class home extends Component {
     );
 
     return (
-      <Grid container spacing={10}>
+      <Grid container spacing={2}>
         <Grid item sm={8} xs={12}>
           {recentScreamsMarkup}
+        </Grid>
+
+        <Grid item sm={4} xs={12}>
+          <Profile />
         </Grid>
       </Grid>
     );
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -6,13 +6,15 @@ import {
   LOADING_UI,
   SET_AUTHENTICATED,
   SET_UNAUTHENTICATED,
+  LOADING_USER,
 } from '../types';
 
 const initialState = {
   authenticated: false,
-  credentails: {},
+  credentials: {},
   like: [],
   notifications: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -28,6 +30,12 @@ export default function (state = initialState, action) {
       return {
         authenticated: true,
         ...action.payload,
+        loading: false,
+      };
+    case LOADING_USER:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
