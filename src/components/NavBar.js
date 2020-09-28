a110259523de571f243f1773a5499e5279d97714
sparsh_parmar <sparshparmar444@gmail.com>
2020-09-28 19:14:11 +0200

    feat: Add NavBar icons acc to Authenticated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Fragement, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 
-class navBar extends Component {
+//Icons
+
+import AddIcon from '@material-ui/icons/Add';
+import HomeIcon from '@material-ui/icons/Home';
+import Notifications from '@material-ui/icons/Notifications';
+
+import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import MyButton from '../util/MyButton';
+import { Typography } from '@material-ui/core';
+class NavBar extends Component {
   render() {
+    const { authenticated } = this.props;
+
     return (
       <AppBar>
         <Toolbar className="nav-container">
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/signUp">
-            SignUp
-          </Button>
+          {authenticated ? (
+            <Fragment>
+              <MyButton tip="Create a Post">
+                <AddIcon color="inherit" />
+              </MyButton>
+
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon color="inherit" />
+                </MyButton>
+              </Link>
+
+              <MyButton tip="Notifications">
+                <Notifications color="inherit" />
+              </MyButton>
+            </Fragment>
+          ) : (
+            <MyButton tip="Notifications">
+              <Notifications color="inherit" />
+            </MyButton>
+          )}
         </Toolbar>
       </AppBar>
     );
   }
 }
 
-export default navBar;
+const mapStateToProps = (state) => ({
+  authenticated: state.user.authenticated,
+});
+
+NavBar.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps)(NavBar);
